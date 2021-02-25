367426ad46d13f680d80715bb1ac6ccaf4911c28
ibrag8998 <ibragdzh@gmail.com>
2021-02-25 14:47:03 +0300

    Update full_name field on user model

--- app/users/models.py
+++ app/users/models.py
@@ -8,9 +8,11 @@ class User(AbstractUser):
     first_name = None
     last_name = None
 
-    full_name = models.CharField(_("full name"), max_length=254)
+    full_name = models.CharField(_("full name"), max_length=254, blank=True, null=True)
 
     last_request = models.DateTimeField(_("last request to the service"), auto_now_add=True)
+
+    REQUIRED_FIELDS = []
 
     def update_last_request(self):
         self.last_request = timezone.now()
